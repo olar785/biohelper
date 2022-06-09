251b035c4f54c183e17b68645a62c5ccb0d4591c
olar785 <olar785@aucklanduni.ac.nz>
2022-06-09 14:27:45 +1200

    Small edit

--- R/pop_taxa.R
+++ R/pop_taxa.R
@@ -13,6 +13,6 @@
 
 pop_taxa = function(ps_obj, undesiredTaxa){
   allTaxa = taxa_names(ps_obj)
-  allTaxa <- allTaxa[!(allTaxa %in% undesiredTaxa)]
-  return(prune_taxa(undesiredTaxa, ps_obj))
+  myTaxa <- allTaxa[!(allTaxa %in% undesiredTaxa)]
+  return(prune_taxa(myTaxa, ps_obj))
 }
