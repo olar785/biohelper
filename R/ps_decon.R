b46f5aee4e0a32614979a9efe614dad5eddd76c8
olar785 <olar785@aucklanduni.ac.nz>
2024-04-27 21:00:26 +1200

    Update on taxo_assignment_summary, which now provides percent of assigned taxa per taxonomic rank. The function ps_decon has also been updated to handle cases where the sample_id is not provided. In addition, ps_decon now has an option to discard samples that do not meet a minimum number of reads. This can help avoid some issues where some samples have very low read counts.

--- R/ps_decon.R
+++ R/ps_decon.R
@@ -10,7 +10,7 @@
 #' The 'max_v' option subtract read associated to putative contaminant ASV by using
 #' their max read count in blank(s).
 #'
-#' Finally, the 'complete_asv_removal' removes all ASVs found all blanks from the dataset.
+#' Finally, the 'complete_asv_removal' removes all ASVs found in blanks from the dataset.
 #'
 #' Note that the 'max_v' option does not take into account the compositional nature of the data.
 #'
@@ -24,7 +24,7 @@
 #' non blank samples must be labeled as 'sample'. Alternatively, the user can provide a list of samples
 #' to use as controls.
 #'
-#' If you use the decon function for a publication, please cite the creators of the microDecon R package (citation('microDecon')).
+#' If you use the decon function for publication, please cite the authors of the microDecon R package (citation('microDecon')).
 
 #'
 #'For example:\cr
@@ -51,115 +51,170 @@
 #' @examples
 #' ps_decon(ps_test_data, method = "microDecon", group = "extraction_method")
 
-ps_decon = function(ps, method= "complete_asv_removal", group=NA, runs=2, thresh = 0.7, prop.thresh = 0.00005, regression = 0, low.threshold=40, up.threshold=400){
-  # Creating custom microDecon function
-  microDecon_2_phyloseq = function(ps_obj, env, decontaminated, taxo_ranks=NULL){
-    if("Mean.blank" %in% colnames(decontaminated$decon.table)){
-      otu_table_ps = otu_table(decontaminated$decon.table[colnames(decontaminated$decon.table) %ni% c("OTU_ID","Mean.blank","Taxonomy")], taxa_are_rows = T)
-    }else{
-      otu_table_ps = otu_table(decontaminated$decon.table[colnames(decontaminated$decon.table) %ni% c("OTU_ID","Taxonomy")], taxa_are_rows = T)
+ps_decon = function (ps_temp, method = "complete_asv_removal", group = NA, runs = 2,
+                     thresh = 0.7, prop.thresh = 5e-05, regression = 0, low.threshold = 40,
+                     up.threshold = 400, min_reads = 10)
+{
+  microDecon_2_phyloseq = function(ps_obj, env, decontaminated,
+                                   taxo_ranks = NULL) {
+    if ("Mean.blank" %in% colnames(decontaminated$decon.table)) {
+      otu_table_ps = otu_table(decontaminated$decon.table[colnames(decontaminated$decon.table) %ni%
+                                                            c("OTU_ID", "Mean.blank", "Taxonomy")], taxa_are_rows = T)
+    }
+    else {
+      otu_table_ps = otu_table(decontaminated$decon.table[colnames(decontaminated$decon.table) %ni%
+                                                            c("OTU_ID", "Taxonomy")], taxa_are_rows = T)
     }
     rownames(otu_table_ps) = decontaminated$decon.table$OTU_ID
-    if(!is.null(taxo_ranks)){
-      tax_ps = tax_table(decontaminated$decon.table$Taxonomy %>% colsplit(";", names = taxo_ranks) %>% as.matrix())
+    if (!is.null(taxo_ranks)) {
+      tax_ps = tax_table(decontaminated$decon.table$Taxonomy %>%
+                           colsplit(";", names = taxo_ranks) %>% as.matrix())
       rownames(tax_ps) = decontaminated$decon.table$OTU_ID
-      ps_trimmed = merge_phyloseq(otu_table_ps,tax_ps)
+      ps_trimmed = merge_phyloseq(otu_table_ps, tax_ps)
       colnames(ps_trimmed@tax_table) = taxo_ranks
-    }else{
+    }
+    else {
       ps_trimmed = otu_table_ps
     }
-    if(!is.null(ps_obj@refseq)){
-      # Adding back the sequences
-      ps_taxa_trimmed = prune_taxa(ps_trimmed %>% taxa_names(), ps_obj)
+    if (!is.null(ps_obj@refseq)) {
+      ps_taxa_trimmed = prune_taxa(ps_trimmed %>% taxa_names(),
+                                   ps_obj)
       fasta_ASVs = ps_taxa_trimmed@refseq
-      fasta_ASVs = fasta_ASVs[match(ps_taxa_trimmed %>% taxa_names(), fasta_ASVs@ranges@NAMES),]
-      ps_trimmed = merge_phyloseq(ps_trimmed,phyloseq::refseq(fasta_ASVs))
+      fasta_ASVs = fasta_ASVs[match(ps_taxa_trimmed %>%
+                                      taxa_names(), fasta_ASVs@ranges@NAMES), ]
+      ps_trimmed = merge_phyloseq(ps_trimmed, phyloseq::refseq(fasta_ASVs))
     }
-    # Adding back the environmental data
-    env=env[rownames(env) %in% sample_names(ps_trimmed),]
-    env=env[match(sample_names(ps_trimmed),rownames(env)),]
+    env = env[rownames(env) %in% sample_names(ps_trimmed),
+    ]
+    env = env[match(sample_names(ps_trimmed), rownames(env)),
+    ]
     sample_data(ps_trimmed) = sample_data(env)
-    ps_trimmed = ps_trimmed %>%
-      phyloseq::subset_samples(amplicon_type=="sample") %>%
-      phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE)
+    ps_trimmed = ps_trimmed %>% phyloseq::subset_samples(amplicon_type ==
+                                                           "sample") %>% phyloseq::filter_taxa(function(x) sum(x) >
+                                                                                                 0, TRUE)
     return(ps_trimmed)
   }
-
-  # Ensuring sample_id and amplicon_type are present in the metadata
-  if(is.null(ps@sam_data$amplicon_type)){
+  if (is.null(ps_temp@sam_data$amplicon_type)) {
     cat("\n")
-    cat("The 'amplicon_type' column is missing from the metadata. Please indicate which rows are 'samples' and which are 'blanks' under 'amplicon_type'.","\n")
-    stop()
+    stop("The 'amplicon_type' column is missing from the metadata. Please indicate which rows are 'samples' and which are 'blanks' under 'amplicon_type'.",
+         "\n")
   }
-  if(is.null(ps@sam_data$sample_id)){
+  if (is.null(ps_temp@sam_data$sample_id)) {
     cat("\n")
-    cat("The 'sample_id' column is missing from the metadata. Adding it to the phyloseq object using sample_names.","\n")
-    ps@sam_data$sample_id = sample_names(ps)
-    stop()
+    cat("The 'sample_id' column is missing from the metadata. Adding it to the phyloseq object using sample_names.",
+        "\n")
+    ps_temp@sam_data$sample_id = sample_names(ps_temp)
   }
+  if (all(ps_temp@sam_data$sample_id != sample_names(ps_temp))) {
+    cat("\n")
+    cat("The 'sample_id' column is present in the metadata but does not match sample names from the phyloseq object. Changing the name of the original sample_id column to 'original_sample_id' and replacing it by sample names of the phyloseq object",
+        "\n")
+    ps_temp@sam_data$original_sample_id = ps_temp@sam_data$sample_id
+    ps_temp@sam_data$sample_id = sample_names(ps_temp)
+  }
+  ps_temp = prune_samples(sample_sums(ps_temp) > 0, ps_temp) %>% phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE)
+  ASVs_in_Blanks = ps_temp %>% phyloseq::subset_samples(amplicon_type != "sample") %>%
+    phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE) %>%
+    phyloseq::taxa_names()
+  if (method == "microDecon") {
+    # It may be necessary to increase min_reads if decon throws the following error: Error in if (((sum(pop.a[b, 1:(ncol(pop.a))]))/group.sum[a]) < prop.thresh) { : missing value where TRUE/FALSE needed
+    ps_temp = prune_samples(sample_sums(ps_temp) > min_reads, ps_temp) %>% phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE)
 
-  # Ensuring no empty sample exist
-  ps = prune_samples(sample_sums(ps) > 0, ps) %>% phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE)
-  ASVs_in_Blanks = ps %>% phyloseq::subset_samples(amplicon_type != "sample") %>% phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE) %>% phyloseq::taxa_names()
-
-  # Processing according to the chosen method
-  if(method == "microDecon"){
-    #ps@sam_data$amplicon_type = suppressWarnings(str_replace_all(ps@sam_data$amplicon_type, pattern = c("NA","na","Na","NaN","nan",""), replacement = NA_character_))
-    ps@sam_data$amplicon_type = ps@sam_data$amplicon_type %>% tolower()
-    metadata = theseus::pstoveg_sample(ps) %>% dplyr::arrange(amplicon_type)
-    ASV_table_ps = theseus::pstoveg_otu(ps) %>% t() %>% as.data.frame()
-    ASV_table_ps = ASV_table_ps[,match(metadata$sample_id, names(ASV_table_ps))] %>% rownames_to_column('OTU_ID')
-    names_blanks_ps = c("OTU_ID",ps %>% subset_samples(amplicon_type!="sample") %>% sample_names())
-    names_samples_ps = ps %>% subset_samples(amplicon_type=="sample") %>% sample_names()
-    ASV_table_ps <- subset(ASV_table_ps, select=c(names_blanks_ps,names_samples_ps))
-    if(!is.null(ps@tax_table)){
-      Taxo=as.data.frame(ps@tax_table@.Data); Taxo$OTU_ID = rownames(Taxo)
-      sorted = Taxo %>% unite("Taxonomy", 1:(ncol(Taxo)-1),sep = ";")
-      ASV_table_ps = ASV_table_ps[match(sorted$OTU_ID,ASV_table_ps$OTU_ID),]
+    ps_temp@sam_data$amplicon_type = ps_temp@sam_data$amplicon_type %>%
+      tolower()
+    metadata = theseus::pstoveg_sample(ps_temp) %>% dplyr::arrange(amplicon_type)
+    ASV_table_ps = theseus::pstoveg_otu(ps_temp) %>% t() %>%
+      as.data.frame()
+    ASV_table_ps = ASV_table_ps[, match(metadata$sample_id,
+                                        names(ASV_table_ps))] %>% rownames_to_column("OTU_ID")
+    names_blanks_ps = c("OTU_ID", ps_temp %>% subset_samples(amplicon_type !=
+                                                               "sample") %>% sample_names())
+    names_samples_ps = ps_temp %>% subset_samples(amplicon_type ==
+                                                    "sample") %>% sample_names()
+    ASV_table_ps <- subset(ASV_table_ps, select = c(names_blanks_ps,
+                                                    names_samples_ps))
+    if (!is.null(ps@tax_table)) {
+      Taxo = as.data.frame(ps_temp@tax_table@.Data)
+      Taxo$OTU_ID = rownames(Taxo)
+      sorted = Taxo %>% unite("Taxonomy", 1:(ncol(Taxo) -
+                                               1), sep = ";")
+      ASV_table_ps = ASV_table_ps[match(sorted$OTU_ID,
+                                        ASV_table_ps$OTU_ID), ]
       ASV_table_ps$Taxonomy = sorted$Taxonomy
     }
-    df_temp = metadata %>% group_by_at(which(colnames(metadata) %in% c("amplicon_type",group))) %>% dplyr::count()
-    #MicroDecon function
-    if(!is.null(ps@tax_table)){
-      decontaminated_ext <- microDecon::decon(data = ASV_table_ps, numb.blanks=sum(df_temp[df_temp$amplicon_type!="sample",]$n), numb.ind = df_temp[df_temp$amplicon_type=="sample",]$n, taxa = TRUE,runs, thresh, prop.thresh, regression, low.threshold, up.threshold)
-      ps_trimmed = microDecon_2_phyloseq(ps_obj = ps, env = theseus::pstoveg_sample(ps), decontaminated = decontaminated_ext, taxo_ranks = colnames(Taxo)[(colnames(Taxo)!="OTU_ID")])
-    }else{
-      decontaminated_ext <- microDecon::decon(data = ASV_table_ps, numb.blanks=sum(df_temp[df_temp$amplicon_type!="sample",]$n), numb.ind = df_temp[df_temp$amplicon_type=="sample",]$n, taxa = FALSE,runs, thresh, prop.thresh, regression, low.threshold, up.threshold)
-      ps_trimmed = microDecon_2_phyloseq(ps_obj = ps, env = theseus::pstoveg_sample(ps), decontaminated = decontaminated_ext)
-    }
+    df_temp = metadata %>% group_by_at(which(colnames(metadata) %in%
+                                               c("amplicon_type", group))) %>% dplyr::count() %>%
+      dplyr::arrange(str_detect(amplicon_type, 'sample'))
 
-  }else if(method == "max_v"){
-    ps_blank = ps %>% subset_samples(amplicon_type!="sample")
-    Extraction_neg_max_vec <- apply(ps_blank %>% theseus::pstoveg_otu() %>% t() %>% as.data.frame(), 1, max) %>% as.vector()
+    if (!is.null(ps@tax_table)) {
+      decontaminated_ext <- microDecon::decon(data = ASV_table_ps,
+                                              numb.blanks = sum(df_temp[df_temp$amplicon_type !=
+                                                                          "sample", ]$n), numb.ind = df_temp[df_temp$amplicon_type ==
+                                                                                                               "sample", ]$n, taxa = TRUE, runs, thresh,
+                                              prop.thresh, regression, low.threshold, up.threshold)
+      ps_trimmed = microDecon_2_phyloseq(ps_obj = ps_temp,
+                                         env = theseus::pstoveg_sample(ps_temp), decontaminated = decontaminated_ext,
+                                         taxo_ranks = colnames(Taxo)[(colnames(Taxo) !=
+                                                                        "OTU_ID")])
+    }
+    else {
+      decontaminated_ext <- microDecon::decon(data = ASV_table_ps,
+                                              numb.blanks = sum(df_temp[df_temp$amplicon_type !=
+                                                                          "sample", ]$n), numb.ind = df_temp[df_temp$amplicon_type ==
+                                                                                                               "sample", ]$n, taxa = FALSE, runs, thresh,
+                                              prop.thresh, regression, low.threshold, up.threshold)
+      ps_trimmed = microDecon_2_phyloseq(ps_obj = ps_temp,
+                                         env = theseus::pstoveg_sample(ps_temp), decontaminated = decontaminated_ext)
+    }
+  }
+  else if (method == "max_v") {
+    ps_blank = ps_temp %>% subset_samples(amplicon_type != "sample")
+    Extraction_neg_max_vec <- apply(ps_blank %>% theseus::pstoveg_otu() %>%
+                                      t() %>% as.data.frame(), 1, max) %>% as.vector()
     names(Extraction_neg_max_vec) = taxa_names(ps_blank)
-    Extractiondf = ps %>% theseus::pstoveg_otu() %>% as.data.frame() %>% dplyr::select(ASVs_in_Blanks)
-    Extractiondf = sweep(Extractiondf,MARGIN=2,Extraction_neg_max_vec,FUN="-")
-    Extractiondf <- replace(Extractiondf, Extractiondf < 0, 0)
-    new_df = ps %>% theseus::pstoveg_otu() %>% as.data.frame() %>% dplyr::select(!ASVs_in_Blanks)
+    Extractiondf = ps_temp %>% theseus::pstoveg_otu() %>% as.data.frame() %>%
+      dplyr::select(ASVs_in_Blanks)
+    Extractiondf = sweep(Extractiondf, MARGIN = 2, Extraction_neg_max_vec,
+                         FUN = "-")
+    Extractiondf <- replace(Extractiondf, Extractiondf <
+                              0, 0)
+    new_df = ps_temp %>% theseus::pstoveg_otu() %>% as.data.frame() %>%
+      dplyr::select(!ASVs_in_Blanks)
     new_df = cbind(new_df, Extractiondf)
-    ps_trimmed = ps
-    ps_trimmed@otu_table = otu_table(new_df, taxa_are_rows=FALSE)
-    ps_trimmed = ps_trimmed %>% subset_samples(amplicon_type == "sample") %>% phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE)
-  } else{
-    ps_trimmed = ps %>% subset_samples(amplicon_type == "sample") %>% phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE)
+    ps_trimmed = ps_temp
+    ps_trimmed@otu_table = otu_table(new_df, taxa_are_rows = FALSE)
+    ps_trimmed = ps_trimmed %>% subset_samples(amplicon_type ==
+                                                 "sample") %>% phyloseq::filter_taxa(function(x) sum(x) >
+                                                                                       0, TRUE)
+  }
+  else {
+    ps_trimmed = ps_temp %>% subset_samples(amplicon_type ==
+                                              "sample") %>% phyloseq::filter_taxa(function(x) sum(x) >
+                                                                                    0, TRUE)
     ps_trimmed = ps_trimmed %>% pop_taxa(ASVs_in_Blanks)
   }
-  # Printing results
-  ntaxa_before = ps %>% subset_samples(amplicon_type == "sample") %>% phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE) %>% ntaxa()
+  ntaxa_before = ps_temp %>% subset_samples(amplicon_type == "sample") %>%
+    phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE) %>%
+    ntaxa()
   ntaxa_after = ntaxa(ps_trimmed)
-
   cat("\n")
-  cat(paste0("Contamination removal outcome using ",method),"\n")
-  cat(paste0("Number of ASVs totally removed: ",ntaxa_before - ntaxa_after))
+  cat(paste0("Contamination removal outcome using ", method),
+      "\n")
+  cat(paste0("Number of ASVs totally removed: ", ntaxa_before -
+               ntaxa_after))
   cat("\n")
-  cat(paste0("Percent of ASVs removed: ",round((1 - (ntaxa_after / ntaxa_before)) * 100,2), " %"))
+  cat(paste0("Percent of ASVs removed: ", round((1 - (ntaxa_after/ntaxa_before)) *
+                                                  100, 2), " %"))
   cat("\n")
-
-  reads_before = ps %>% subset_samples(amplicon_type == "sample") %>% phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE) %>% sample_sums() %>% sum()
-  reads_after = ps_trimmed %>% phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE) %>% sample_sums() %>% sum()
-
-  cat(paste0("Total number of reads  removed: ",reads_before - reads_after))
-  cat("\nPercent of reads removed: ",paste0(round((1 - (reads_after / reads_before)) * 100,2)," %"))
+  reads_before = ps_temp %>% subset_samples(amplicon_type == "sample") %>%
+    phyloseq::filter_taxa(function(x) sum(x) > 0, TRUE) %>%
+    sample_sums() %>% sum()
+  reads_after = ps_trimmed %>% phyloseq::filter_taxa(function(x) sum(x) >
+                                                       0, TRUE) %>% sample_sums() %>% sum()
+  cat(paste0("Total number of reads  removed: ", reads_before -
+               reads_after))
+  cat("\nPercent of reads removed: ", paste0(round((1 - (reads_after/reads_before)) *
+                                                     100, 2), " %"))
   cat("\n")
   return(ps_trimmed)
 }
--- R/taxo_assignment_summary.R
+++ R/taxo_assignment_summary.R
@@ -1,22 +1,41 @@
-#' taxo_assingment_summary
+#' taxo_assignment_summary
 #'
-#' This function takes in a dataframe of taxonomic assignment and returns the mean number and standard deviation of taxonomic ranks assigned.
+#' This function takes in a phyloseq object with taxonomy or a dataframe of taxonomic assignment and returns the percent of assigned taxa per taxonomic rank, and the mean number and standard deviation of taxonomic rank assigned.
 #'
 #' @param
-#' df Dataframe of taxonomic assignment
+#' obj A phyloseq object with taxonomy or a dataframe of taxonomic assignment
 #' @param
-#' ranks  Taxonomic ranks in the dataframe
+#' ranks  Taxonomic ranks of interest from the taxonomic assignment. By default, the function uses colnames of the tax_table of the phyloseq object or of the dataframe, depending on what is provided.
+#' print_only If TRUE (default), results are printed only. If FALSE, a dataframe is returned.
 #'
 #' @export
 #' @examples
 #' df = ps_test_data@tax_table %>% as.data.frame()
 #' taxo_assignment_summary(df = df, ranks = c("Kingdom","Phylum","Class","Order","Family","Genus"))
 
-taxo_assignment_summary = function(df, ranks){
-  df = df %>%
-    as.data.table()
+taxo_assignment_summary = function(obj, ranks = NA, print_only = T){
+  if(any(class(obj) %in% "phyloseq")){
+    df = obj@tax_table@.Data %>% as.data.table()
+  }else{
+    df = obj %>% as.data.table()
+  }
+  if(any(is.na(ranks))){
+    ranks = colnames(df)[tolower(colnames(df)) %ni% c("asv", "otu")]
+  }
   df[df==""]<-NA
-  df$nRb = length(ranks) - rowSums(is.na(df %>% dplyr::select(ranks)))
-  temp_summary = df %>% dplyr::summarise(mean = round(mean(nRb),2), sd = round(sd(nRb),2))
-  cat("\nMean assigned taxonomic ranks: ",temp_summary$mean %>% as.numeric(),"\nStandard deviation: ",temp_summary$sd %>% as.numeric(),"\n\n")
+  df$nRb = length(ranks) - rowSums(is.na(df %>% dplyr::select(all_of(ranks))))
+  temp_summary_overall = df %>% dplyr::summarise(mean = round(mean(nRb),2), sd = round(sd(nRb),2))
+  temp_summary_per_rank = df  %>%
+    pivot_longer(cols = ranks, names_to = "taxonomic_rank", values_to = "taxa") %>%
+    dplyr::mutate(assignment = ifelse(is.na(taxa), 0, 1)) %>%
+    dplyr::group_by(taxonomic_rank) %>%
+    dplyr::summarise(percent_assigned = round(sum(assignment) / nrow(df),2)*100) %>%
+    dplyr::arrange(desc(percent_assigned))
+
+  if(print_only != T){
+    return(temp_summary_per_rank)
+  }else{
+    print(temp_summary_per_rank)
+  }
+  cat("\nMean number of assigned taxonomic rank: ",temp_summary_overall$mean %>% as.numeric(),"( out of",length(ranks),")","\nStandard deviation: ",temp_summary$sd %>% as.numeric(),"\n\n")
 }
